b2312310f8774df58aaf36f4efdb914c77681836
Henrik Jordheim <henrjor@stud.ntnu.no>
2021-04-19 18:18:51 +0200

    Starting out on handwritten numbers

---
+++ MNist_ttt4275/handwritten_numbers.m
@@ -0,0 +1,15 @@
+clc;
+clear;
+%% Initializing and set up
+load('data_all.mat');
+C = 10;
+% ref = eye(10); % Blir kanskje ikke helt rett....
+
+
+%% NN-based classifier with Euclidian distance, using whole training set as templates
+
+% 
+% D = 0;
+% for i = 1:num_train
+%     D = D + ()
+% end
